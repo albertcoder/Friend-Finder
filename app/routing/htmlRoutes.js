64e872b76cb1526a14d76ff8a32301f1a79f9be0
Albert Coder <eralbert9191@gmail.com>
2018-11-15 13:54:33 -0800

    added the HTML routes for handling URL hits

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+// Required path dependency
+var path = require('path');
+
+module.exports = function (app) {
+    // get route to load survey.html
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    // default route to homepage
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
