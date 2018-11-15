4bfaf31f035c4a13358ed07fe6c554be3bfa18b9
Albert Coder <eralbert9191@gmail.com>
2018-11-15 13:57:12 -0800

    handled the api routes and logic for finding the maximum compatibility with friends using user inputs

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,51 @@
+// require data from friends.js
+var friends = require("../data/friends");
+
+module.exports = function (app) {
+    // GET request to API of all friends
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
+    // POST request to add newFriend object data to friends array
+    app.post("/api/friends", function (req, res) {
+        // store data from user survey in variable newFriend
+        var newFriend = req.body;
+        // CALC SUM OF NEW FRIEND SCORES
+        // convert survey responses to integers
+        var newFriendTotalScore = 0;
+        var newFriendScoreArr = [];
+        for (var j = 0; j < newFriend.scores.length; j++) {
+            newFriend.scores[j] = parseInt(newFriend.scores[j]);
+            newFriendTotalScore = newFriendTotalScore + newFriend.scores[j];
+        }
+        newFriendScoreArr.push(newFriendTotalScore);
+        console.log(newFriend.name + ": " + newFriendTotalScore);
+
+        // CALC DIFFERENCE BETWEEN SUM OF FRIENDS SCORES AND USER SCORES
+        var diffArr = [];
+        for (var i in friends) {
+            // declare variable to store scores of friends currently in DB
+            var dbFriendScores = friends[i].scores;
+            var dbFriendTotalScore = 0;
+            // ITERATE THROUGH SCORES OF EACH FRIEND
+            for (var k = 0; k < dbFriendScores.length; k++) {
+                dbFriendScores[k] = parseInt(dbFriendScores[k]);
+                dbFriendTotalScore = dbFriendTotalScore + dbFriendScores[k];
+            }
+            // STORE DIFFERENCE BETWEEN SCORES IN AN ARRAY
+            var diff = newFriendScoreArr - dbFriendTotalScore;
+            // IF DIFF IS NEGATIVE CONVERT TO POSITIVE VALUE
+            if (diff < 0) {
+                diff = diff * -1;
+                diffArr.push(diff);
+            } else {
+                diffArr.push(diff);
+            }
+        }
+        console.log(diffArr);
+        // FIND INDEX OF SMALLEST DIFFERENCE IN TOTAL SCORES
+        var bestFriendIndex = diffArr.indexOf(Math.min(...diffArr));
+        // RETURN THE 'BEST FRIEND' in modal
+        res.json(friends[bestFriendIndex]);
+    });
+};
