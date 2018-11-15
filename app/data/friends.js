9b2ba2cb08a94af491f5fc5399b23dbcd0356def
Albert Coder <eralbert9191@gmail.com>
2018-11-15 13:51:21 -0800

    added the friends data in friends.js file as an array of friends

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+// Friends data stored in an array
+
+var friends = [
+    {
+        name: "Albert",
+        photo: "https://avatars0.githubusercontent.com/u/6724613?s=460&v=4",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Kate Middleton",
+        photo: "https://media1.popsugar-assets.com/files/thumbor/sRNnbQZjo6BA6n2zfXGthkSFnMQ/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/07/13/034/n/1922398/3257e29f5b493aa8881266.30084379_/i/Kate-Middleton.jpg",
+        scores: [3,3,3,3,2,4,3,2,1,2]
+    },
+    {
+        name: "Tom Cruise",
+        photo: "http://images5.fanpop.com/image/photos/27800000/Mission-Impossible-II-2000-tom-cruise-27899157-666-1000.jpg",
+        scores: [5,4,4,3,5,5,4,5,5,5]
+    },
+    {
+        name: "Elon Musk",
+        photo: "https://static.dezeen.com/uploads/2015/10/Elon-Musk-portrait_Tesla_Apple_dezeen_sq.jpg",
+        scores: [4,2,3,1,3,2,4,1,1,5]
+    }
+];
+
+module.exports = friends;
+
